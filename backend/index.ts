149c72a095e4d0add2c73a3ebdcda8c70e6f57f1
Werapat <hellp002@gmail.com>
2024-01-28 03:45:32 +0700

    feat : add send email method on backend

--- backend/index.ts
+++ backend/index.ts
@@ -1,4 +1,5 @@
 import express, { Express, Request, Response } from "express";
+import send_mail from "./lib/mail_sender";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -6,10 +7,21 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3001;
 
+app.use(express.json());
+//app.use(express.urlencoded());
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, this is Express + TypeScript server");
 });
 
+app.get('/api/email',(req : Request, res : Response) => {
+  res.send("Email sender");
+});
+
+app.post("/api/email",send_mail);
+
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
+
+module.exports = app;
---
+++ backend/lib/mail_sender.ts
@@ -0,0 +1,59 @@
+import { Request, Response } from 'express';
+import nodemailer from 'nodemailer';
+import z from 'zod';
+
+const Schema_DataSender = z.object({
+    "from" : z.string().email("Invalid email in section from"),
+    "to" : z.string().email("Invalid email in section to"),
+    "subject" : z.coerce.string().min(1,{message : "There is no subject in this form"}),
+    "text" : z.string().optional().or(z.literal('')),
+});
+
+type DataSender = z.infer<typeof Schema_DataSender>;
+
+const send_mail =async (req : Request,res : Response) => {
+    const {from,to,subject,text} = req.body;
+
+    const data = {
+        "from" : from,
+        "to" : to,
+        "subject" : subject,
+        "text" : text
+    }
+    const data_parse = Schema_DataSender.safeParse(data);
+    if(!data_parse.success) {
+        const my_message : string[] = [];
+        for (const mes of data_parse.error.errors){
+            const message = mes.message;
+            my_message.push(message);    
+        } 
+        return res.status(400).send({message : my_message.join("\nAnd ")});
+    }
+    const mail : DataSender = data_parse.data;
+    const result = await sender(mail);
+    res.send(result);
+    
+}
+
+const sender = async (data : DataSender) =>{
+    const transporter = nodemailer.createTransport({
+      service:"gmail",
+      port: 465,               // true for 465, false for other ports
+      host: "smtp.gmail.com",
+         auth: {
+              user: 'horhub48@gmail.com',
+              pass: 'vvlcdqxirywwcbch',
+           },
+      secure: true,
+    });
+    transporter.sendMail(data,(err,info)=>{
+        if(err){
+          console.log(err);
+        }
+        else {
+          return info.response;
+        }
+    });};
+
+
+export default send_mail;
