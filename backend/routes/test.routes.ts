01bcf5c8c9de95929aee52b781e6744d498d3160
creampiney <sorawit12535@gmail.com>
2024-01-28 11:28:42 +0700

    setup: simple router

---
+++ backend/routes/test.routes.ts
@@ -0,0 +1,10 @@
+import express, { Express, Request, Response } from "express";
+import { Router } from "express";
+
+const router = Router()
+
+router.get('/hello', (req, res) => {
+    res.send("Hello World!")
+})
+
+export default router
