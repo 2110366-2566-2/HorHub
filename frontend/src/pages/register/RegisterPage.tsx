45c226a80b385857024af70e86934b830cd7a07e
creampiney <sorawit12535@gmail.com>
2024-02-07 18:12:47 +0700

    feat: validate form register

--- frontend/src/pages/register/RegisterPage.tsx
+++ frontend/src/pages/register/RegisterPage.tsx
@@ -6,71 +6,54 @@ import { FaTransgenderAlt } from "react-icons/fa";
 import { IoIosMail } from "react-icons/io";
 import { MdLock } from "react-icons/md";
 import { MdManageAccounts } from "react-icons/md";
+import { z } from "zod";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod';
+
+
+
+
 
 export default function RegisterPage() {
-  
-  function toRegister(registrationForm:FormData) {
-    const firstname = registrationForm.get("firstname")?.toString()
-    const lastname = registrationForm.get("lastname")?.toString()
-    const displayName = registrationForm.get("displayName")?.toString()
-    const phoneNumber = registrationForm.get("phoneNumber")?.toString()
-    const birthDate = registrationForm.get("birthDate")?.toString()
-    const gender = registrationForm.get("gender")?.toString()
-    const email = registrationForm.get("email")?.toString()
-    const password = registrationForm.get("password")?.toString()
-    const role = registrationForm.get("role")?.toString()
-    
-    // Just testing
-    const registrationInfo = [firstname,lastname,role,displayName,phoneNumber,birthDate,
-                              gender,email,password,role]
-    console.log(registrationInfo)
 
-    // To implement backend connection
+  const schema = z.object({
+    email: z.string().trim().email(),
+    password: z.string().trim().min(8, {message: 'Password must be at least 8 characters'}),
+    confirmPassword: z.string().trim().min(8, {message: 'Password must be at least 8 characters'}),
+    firstName: z.string().trim().min(1, {message: 'Fill your first name'}),
+    lastName: z.string().trim().min(1, {message: 'Fill your last name'}),
+    displayName: z.string().trim().min(1, {message: 'Fill display name'}),
+    phoneNumber: z.string().trim().length(10, {message: 'Please fill valid phone number'})
+                  .refine((value) => /[0-9]{10}/.test(value), {message: 'Please fill valid phone number'}),
+    birthDate: z.date()
+    
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ['confirmPassword'],
+    message: 'Password and confirm password does not match'
+  })
+
+  type ValidationSchemaType = z.infer<typeof schema>
+
+  const { register, handleSubmit, formState: { errors } } = useForm<ValidationSchemaType>({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit: SubmitHandler<ValidationSchemaType> = (data) => {
+    console.log(data)
   }
   
   return(
-    <div className="flex flex-col items-center h-screen mb-3">
-      <div className="mt-8 text-2xl font-bold">Register</div>
-      <div className="rounded-xl border border-blue-500 mt-8 h-fit bg-white w-3/5 pb-8">
-        <form onSubmit={(e) => { e.preventDefault(); toRegister(new FormData(e.target as HTMLFormElement)); }} className="mx-10 mt-8 flex flex-col items-center">
-          <div className="grid grid-cols-2 w-full gap-6 gap-y-10">
-            <div className="flex flex-row">
-              <VscAccount className='h-6 w-6 place-self-center text-blue-500' />
-              <input 
-              type="text"
-              name="firstname" 
-              placeholder="Firstname" 
-              className="ml-2 border-0 border-b border-black outline-0 w-full"/>
-            </div> 
-            <input 
-              type="text"
-              name="lastname" 
-              placeholder="Lastname" 
-              className="border-0 border-b border-black outline-0 w-full"/>
-            <div className="flex flex-row">
-              <MdOutlineSupervisorAccount className='h-6 w-6 place-self-center text-blue-500' />
-              <input 
-              type="text"
-              name="displayName" 
-              placeholder="Display name" 
-              className="ml-2 border-0 border-b border-black outline-0 w-full"/>
-            </div> 
-            <div className="flex flex-row">
-              <IoPhonePortraitSharp className='h-6 w-6 place-self-center text-blue-500' />
-              <input 
-              type="text"
-              name="phoneNumber" 
-              placeholder="Phone number" 
-              className="ml-2 border-0 border-b border-black outline-0 w-full"/>
-            </div>
-            <div className="flex flex-row">
-              <RiCake2Line className='h-6 w-6 place-self-center text-blue-500' />
-              <input 
-              type="date"
-              name="birthDate" 
-              placeholder="Birthdate"
-              className="ml-2 border-0 border-b border-black outline-0 w-full"/>
-            </div> 
+    <div className="page">
+      <div className="text-2xl font-bold">Register</div>
+      <div className="rounded-xl border border-blue-500 mt-8 h-fit bg-white w-full md:w-4/5 lg:w-3/5 pb-8 text-sm">
+        <form onSubmit={handleSubmit(onSubmit)} className="mx-10 mt-8 flex flex-col items-center gap-8">
+          <div className="grid grid-cols-1 lg:grid-cols-2 w-full gap-6 gap-y-10">
+            {/* 
+            
+            
+
+            
             <div className="flex flex-row">
               <FaTransgenderAlt className='h-6 w-6 place-self-center text-blue-500' />
               <div className="ml-4 w-full flex gap-4">
@@ -102,24 +85,87 @@ export default function RegisterPage() {
                   <label htmlFor="Other" className="text-sm font-medium ml-2 hover:cursor-pointer">Other</label>
                 </div>
               </div>
+            </div> */}
+            <div className="w-full lg:col-span-2">
+              <span className="font-bold text-base">Authentication Information</span>
             </div>
-            <div className="flex flex-row">
+
+            <div className="flex flex-row lg:col-span-2">
               <IoIosMail className='h-6 w-6 place-self-center text-blue-500' />
               <input 
               type="text"
-              name="email" 
               placeholder="Email address" 
-              className="ml-2 border-0 border-b border-black outline-0 w-full"/>
+              className="ml-2 border-0 border-b border-black outline-0 w-full"
+              {...register('email')} />
             </div>
+
             <div className="flex flex-row">
               <MdLock className='h-6 w-6 place-self-center text-blue-500' />
               <input 
               type="password"
-              name="password" 
               placeholder="Password" 
-              className="ml-2 border-0 border-b border-black outline-0 w-full"/>
+              className="ml-2 border-0 border-b border-black outline-0 w-full"
+              {...register('password')} />
             </div>
+
             <div className="flex flex-row">
+              <MdLock className='h-6 w-6 place-self-center text-blue-500' />
+              <input 
+              type="password"
+              placeholder="Confirm Password" 
+              className="ml-2 border-0 border-b border-black outline-0 w-full"
+              {...register('confirmPassword')} />
+            </div>
+
+            <div className="w-full lg:col-span-2">
+              <span className="font-bold text-base">Account Information</span>
+            </div>
+
+            <div className="flex flex-row lg:col-span-2">
+              <VscAccount className='h-6 w-6 place-self-center text-blue-500' />
+              <input 
+              type="text"
+              placeholder="First Name" 
+              className="ml-2 border-0 border-b border-black outline-0 w-1/2"
+              {...register('firstName')} />
+              <input 
+              type="text"
+              placeholder="Last Name" 
+              className="ml-4 border-0 border-b border-black outline-0 w-1/2"
+              {...register('lastName')} />
+            </div> 
+
+            <div className="flex flex-row">
+              <MdOutlineSupervisorAccount className='h-6 w-6 place-self-center text-blue-500' />
+              <input 
+              type="text"
+              placeholder="Display Name" 
+              className="ml-2 border-0 border-b border-black outline-0 w-full"
+              {...register('displayName')} />
+            </div> 
+
+            <div className="flex flex-row">
+              <IoPhonePortraitSharp className='h-6 w-6 place-self-center text-blue-500' />
+              <input 
+              type="tel"
+              placeholder="Phone Number" 
+              className="ml-2 border-0 border-b border-black outline-0 w-full"
+              {...register('phoneNumber')} />
+            </div>
+            
+            <div className="flex flex-row">
+              <RiCake2Line className='h-6 w-6 place-self-center text-blue-500' />
+              <input 
+              type="date"
+              placeholder="Birthdate"
+              className="ml-2 border-0 border-b border-black outline-0 w-full"
+              {...register('birthDate')} />
+            </div> 
+
+            <div>
+              {errors.birthDate?.message}
+            </div>
+            {/* <div className="flex flex-row">
               <MdManageAccounts className='h-6 w-6 place-self-center text-blue-500' />
               <div className="ml-4 w-full flex gap-4">
                 <div className="hover:cursor-pointer flex items-center">
@@ -141,7 +187,7 @@ export default function RegisterPage() {
                   <label htmlFor="provider" className="text-sm font-medium ml-2 hover:cursor-pointer">Provider</label>
                 </div>
               </div>
-            </div>
+            </div> */}
           </div>
           <button type="submit" className="primary-button">
             Register
