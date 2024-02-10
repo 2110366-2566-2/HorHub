47e17b85d643247f665bd11da8dc96c06bbc9fe0
Werapat <hellp002@gmail.com>
2024-02-11 03:14:44 +0700

    feat : add userinfo type

--- frontend/src/lib/getUser.ts
+++ frontend/src/lib/getUser.ts
@@ -1,13 +1,16 @@
-import Cookies from "js-cookie";
+import { UserInfo, UserInfoSchema } from "./type/UserHidden";
 
-export default async function getUser(){
+
+export default async function getUser() : Promise<UserInfo | null>{
     const jwt = await fetch(process.env.REACT_APP_BACKEND_URL + "/auth/user",{
         method : "GET",
         credentials : "include"
     })
     if (jwt.status === 200){
         const data = await jwt.json();
-        console.log(data);
+        const user_data = UserInfoSchema.safeParse(data);
+        if(!user_data.success) return null;
+        return user_data.data;
     }
     return null;
     
---
+++ frontend/src/lib/type/UserHidden.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const UserInfoSchema = z.object({
+    email: z.string().trim().email(),
+    
+    firstName: z.string().trim().min(1, {message: 'Fill your first name'}),
+    lastName: z.string().trim().min(1, {message: 'Fill your last name'}),
+    displayName: z.string().trim().min(1, {message: 'Fill display name'}),
+    phoneNumber: z.string().trim().length(10, {message: 'Please fill valid phone number'})
+                    .refine((value) => /[0-9]{10}/.test(value), {message: 'Please fill valid phone number'}),
+    birthdate: z.coerce.date(),
+    gender: z.enum(["Male", "Female", "Other"], {invalid_type_error: 'Gender is not valid, gender must be "Male", "Female", or "Other"'}),
+    role: z.enum(["Customer", "Provider"], {invalid_type_error: 'Role is not valid, role must be "Customer" or "Provider"'})
+});
+
+export type UserInfo = z.infer<typeof UserInfoSchema>;
