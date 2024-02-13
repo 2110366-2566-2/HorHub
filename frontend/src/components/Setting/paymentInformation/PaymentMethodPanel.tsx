c3b565bdc7ccfde79011710c497babe46f8fe62b
Sorawit <sorawit12535@gmail.com>
2024-02-13 10:38:05 +0700

    feat: role payment

--- frontend/src/components/Setting/paymentInformation/PaymentMethodPanel.tsx
+++ frontend/src/components/Setting/paymentInformation/PaymentMethodPanel.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import ProviderPaymentMethod from './PaymentMethod/ProviderPaymentMethod'
 import CustomerPaymentMethod from './PaymentMethod/CustomerPaymentMethod'
+import { useUser } from '../../../lib/context/UserContext'
 
 const PaymentMethodPanel = () => {
+  const {currentUser, isLoading} = useUser()
   return (
     <div className="flex flex-col w-full">
         <div className="border-b border-slate-300 my-2 font-bold text-left">Payment Method</div>
-        <ProviderPaymentMethod />
-        <CustomerPaymentMethod />
+        {
+          currentUser && currentUser.role === "Provider" && <ProviderPaymentMethod />
+        }
+        {
+          currentUser && currentUser.role === "Customer" && <CustomerPaymentMethod />
+        }
     </div>
   )
 }
