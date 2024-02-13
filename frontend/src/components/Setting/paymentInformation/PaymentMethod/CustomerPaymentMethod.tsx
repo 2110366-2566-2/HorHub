2b2816beeba8378c062740f091b92573e051b1cf
Sorawit <sorawit12535@gmail.com>
2024-02-13 10:35:08 +0700

    feat: customer table

---
+++ frontend/src/components/Setting/paymentInformation/PaymentMethod/CustomerPaymentMethod.tsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from 'react'
+import { useUser } from '../../../../lib/context/UserContext'
+
+const CustomerPaymentMethod = () => {
+
+    const {currentUser} = useUser()
+
+    const [methodData, setMethodData] = useState<{id: String, type: string, info: string}[]>([])
+
+
+    useEffect(() => {
+        if (!currentUser) {
+            return
+        }
+        fetch(process.env.REACT_APP_BACKEND_URL + "/users/" + currentUser.id + "/paymentMethods", {
+            method: "GET"
+        }).then((res) => res.json())
+        .then((data: {id: string, type: string, info: string}[]) => {setMethodData(data)})
+    }, [currentUser])
+
+
+
+  return (
+    <div className="w-full flex-col space-y-8">
+        <div className="w-full flex-col gap-3">
+            <div className="text-base w-full font-bold text-left">Bank Accounts</div>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th className="w-[5%]"></th>
+                        <th className="w-[15%]">Bank Name</th>
+                        <th>Account Number</th>
+                        <th className="w-[10%]"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        methodData.filter((value) => {return value.type === "Bank"}).map((data, idx) => {
+                            return (
+                                <tr className="hover:bg-slate-100 transition-colors">
+                                    <th>{idx+1}</th>
+                                    <td>{data.info.split('-')[0]}</td>
+                                    <td>{data.info.split('-')[1]}</td>
+                                    <td>Delete?</td>
+                                </tr>
+                            )
+                        })
+                    }
+                    
+                </tbody>
+            </table>
+        </div>
+
+        <div className="w-full flex-col gap-3">
+            <div className="text-base w-full font-bold text-left">Credit or Debit Cards</div>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th className="w-[5%]"></th>
+                        <th>Card Number</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className="hover">
+                        <th>1</th>
+                        <td>1234567891234567</td>
+                        <td>Delete bro?</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+  )
+}
+
+export default CustomerPaymentMethod
--- frontend/src/components/Setting/paymentInformation/PaymentMethodPanel.tsx
+++ frontend/src/components/Setting/paymentInformation/PaymentMethodPanel.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ProviderPaymentMethod from './PaymentMethod/ProviderPaymentMethod'
+import CustomerPaymentMethod from './PaymentMethod/CustomerPaymentMethod'
 
 const PaymentMethodPanel = () => {
   return (
     <div className="flex flex-col w-full">
         <div className="border-b border-slate-300 my-2 font-bold text-left">Payment Method</div>
         <ProviderPaymentMethod />
+        <CustomerPaymentMethod />
     </div>
   )
 }
