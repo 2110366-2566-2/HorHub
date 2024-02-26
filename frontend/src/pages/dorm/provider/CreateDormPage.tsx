a8e2d83e829d73f6f980b41827245bf7f280e522
Sorawit <sorawit12535@gmail.com>
2024-02-26 10:13:31 +0700

    feat: new loading page

--- frontend/src/pages/dorm/provider/CreateDormPage.tsx
+++ frontend/src/pages/dorm/provider/CreateDormPage.tsx
@@ -4,25 +4,35 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 import { z } from 'zod';
 import TextInput from '../../../components/Form/TextInput';
 import TextAreaInput from '../../../components/Form/TextAreaInput';
+import { useUser } from '../../../lib/context/UserContext';
+import LoadingPage from '../../etc/LoadingPage';
 
 const schema = z.object({
     name: z.string().trim().min(1, {message: "Fill dorm name"}).max(100, {message: "Your dorm name must not exceed 100 characters"}),
-    description: z.string().trim().min(1, {message: "Fill description"}).max(5000, {message: "Description must not exceed 5000 characters"})
- })
+    description: z.string().trim().min(1, {message: "Fill description"}).max(5000, {message: "Description must not exceed 5000 characters"}),
+    address: z.string().trim().min(1, {message: "Fill dorm address"}).max(300, {message: "Address must not exceed 300 characters"}),
+    latitude: z.coerce.number().min(-90.00000, {message: "The value must be between -90.00000 to 90.00000"}).max(90.00000, {message: "The value must be between -90.00000 to 90.00000"}),
+    longitude: z.coerce.number().min(-180.00000, {message: "The value must be between -180.00000 to 180.00000"}).max(180.00000, {message: "The value must be between -180.00000 to 180.00000"}),
+})
 
 type ValidationSchemaType = z.infer<typeof schema>;
 
 const CreateDormPage = () => {
+
+    const {currentUser, isLoading} = useUser()
     
     const { register, handleSubmit, formState: { errors } } = useForm<ValidationSchemaType>({
         resolver: zodResolver(schema),
+        mode: 'all'
     });
     
     const onSubmit: SubmitHandler<ValidationSchemaType> = async (data) => {
         console.log(data)
     }
 
-    
+    if (isLoading) {
+        return <LoadingPage />
+    }
 
 
     return (
@@ -48,6 +58,39 @@ const CreateDormPage = () => {
                 error={errors.description} 
             />
 
+            <div className="border-b border-slate-300 my-2 font-bold text-left">Location</div>
+
+            <TextInput 
+                type="text" 
+                name="address" 
+                fieldName="Address" 
+                placeholder="Address..." 
+                register={register} 
+                error={errors.address} 
+            />
+
+            <div className="flex gap-3">
+                <div className="w-40">
+                    <TextInput 
+                        type="number" 
+                        name="latitude" 
+                        fieldName="Latitude" 
+                        placeholder="0.00000" 
+                        register={register} 
+                        error={errors.latitude} 
+                    />
+                </div>
+                <div className="w-40">
+                    <TextInput 
+                        type="number" 
+                        name="longitude" 
+                        fieldName="Longitude" 
+                        placeholder="0.00000" 
+                        register={register} 
+                        error={errors.longitude} 
+                    />
+                </div>
+            </div>
 
             <div className="w-full flex justify-start">
               <button 
--- frontend/src/pages/etc/LoadingPage.tsx
+++ frontend/src/pages/etc/LoadingPage.tsx
@@ -1,7 +1,32 @@
+import './hamsterLoading.css'
+
 function LoadingPage() {
     return (
-        <div className="page">
-            Loading...
+        <div className="page justify-center gap-4">
+            <div className="text-xl">
+                Loading...
+            </div>
+            
+            <div>
+            <div aria-label="Orange and tan hamster running in a metal wheel" role="img" className="wheel-and-hamster">
+	<div className="wheel"></div>
+	<div className="hamster">
+		<div className="hamster__body">
+			<div className="hamster__head">
+				<div className="hamster__ear"></div>
+				<div className="hamster__eye"></div>
+				<div className="hamster__nose"></div>
+			</div>
+			<div className="hamster__limb hamster__limb--fr"></div>
+			<div className="hamster__limb hamster__limb--fl"></div>
+			<div className="hamster__limb hamster__limb--br"></div>
+			<div className="hamster__limb hamster__limb--bl"></div>
+			<div className="hamster__tail"></div>
+		</div>
+	</div>
+	<div className="spoke"></div>
+</div>
+            </div>
         </div>
     )
 }
