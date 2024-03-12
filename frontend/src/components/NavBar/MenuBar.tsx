fa33fe55d8cf46d788798831edc65c74a241fd6c
Sorawit <sorawit12535@gmail.com>
2024-03-12 12:57:43 +0700

    feat: adjust menu bar

--- frontend/src/components/NavBar/MenuBar.tsx
+++ frontend/src/components/NavBar/MenuBar.tsx
@@ -1,5 +1,7 @@
 import { LuUser2 } from "react-icons/lu";
 import { MdLogout, MdOutlineSettings } from "react-icons/md";
+import { MdOutlineSpaceDashboard } from "react-icons/md";
+import { FiBookOpen } from "react-icons/fi";
 import { useNavigate } from "react-router-dom";
 import { useUser } from "../../lib/context/UserContext";
 
@@ -7,12 +9,24 @@ function MenuBar() {
 
     const iconClassName = "h-4 w-4"
     const navigate = useNavigate();
-    const {fetchUser} = useUser();
+    const {currentUser, fetchUser} = useUser();
     const menuList = [
         {
             name: "My Profile",
             icon: <LuUser2 className={iconClassName} />,
             onClick: () => {navigate('/profile')}
+        },
+        {
+            name: "My Reservation",
+            icon: <FiBookOpen className={iconClassName} />,
+            onClick: () => {navigate('/bookings')},
+            allow: "Customer"
+        },
+        {
+            name: "Dashboard",
+            icon: <MdOutlineSpaceDashboard className={iconClassName} />,
+            onClick: () => {navigate('/provider/dorms')},
+            allow: "Provider"
         },
         {
             name: "Settings",
@@ -30,7 +44,8 @@ function MenuBar() {
                 fetchUser();
                 navigate('/');
             }
-        }
+        },
+
     ]
 
 
@@ -39,16 +54,54 @@ function MenuBar() {
             <ul className="menu bg-base-200 w-56 rounded-box text-sm">
                 {
                     menuList.map((data, idx) => {
-                        return (
-                            <li key={idx}>
-                                <button 
-                                    className="w-full flex items-center hover:bg-slate-100"
-                                    onClick={data.onClick}>
-                                    {data.icon}
-                                    <span className="text-sm">{data.name}</span>
-                                </button>
-                            </li>
-                        )
+                        if (!data.allow || !currentUser) {
+                            return (
+                                <li key={idx}>
+                                    <button 
+                                        className="w-full flex items-center hover:bg-slate-100"
+                                        onClick={data.onClick}>
+                                        {data.icon}
+                                        <span className="text-sm">{data.name}</span>
+                                    </button>
+                                </li>
+                            )
+                        }
+                        if (data.allow === "Customer") {
+                            if (currentUser.role === "Customer") {
+                                return (
+                                    <li key={idx}>
+                                        <button 
+                                            className="w-full flex items-center hover:bg-slate-100"
+                                            onClick={data.onClick}>
+                                            {data.icon}
+                                            <span className="text-sm">{data.name}</span>
+                                        </button>
+                                    </li>
+                                )
+                                
+                            }
+                            else {
+                                return <></>
+                            }
+                        }
+                        else if (data.allow === "Provider") {
+                            if (currentUser.role === "Provider") {
+                                return (
+                                    <li key={idx}>
+                                        <button 
+                                            className="w-full flex items-center hover:bg-slate-100"
+                                            onClick={data.onClick}>
+                                            {data.icon}
+                                            <span className="text-sm">{data.name}</span>
+                                        </button>
+                                    </li>
+                                )
+                                
+                            }
+                            else {
+                                return <></>
+                            }
+                        }
                     })
                 }
                 
