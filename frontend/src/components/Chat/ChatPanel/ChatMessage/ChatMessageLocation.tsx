e4f8e60de84a61b271cea2325294f0c30f8f3de7
jutatuch <jutatuch777@gmail.com>
2024-03-26 13:37:57 +0700

    fix!!: map loading when refreshing

--- frontend/src/components/Chat/ChatPanel/ChatMessage/ChatMessageLocation.tsx
+++ frontend/src/components/Chat/ChatPanel/ChatMessage/ChatMessageLocation.tsx
@@ -1,14 +1,27 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import DormMap from '../../../Dorm/DormMap'
 import { Box, Modal } from '@mui/material'
 
 const ChatMessageLocation = ({ latitude, longitude }: { latitude: number, longitude: number }) => {
   const [open, setOpen] = useState<boolean>(false)
 
+  // const [tempLat, setTempLat] = useState<number>(0)
+  // const [tempLng, setTempLng] = useState<number>(0)
+
+  const [tempState, setTempState] = useState<boolean>(false)
+
+  useEffect(() => {
+    setTempState(false)
+    setTimeout(() => {
+      setTempState(true)
+    }, 80)
+  }, [])
+
+
   return (
     <>
       <div onClick={() => setOpen(true)} className="w-72 aspect-video">
-          <DormMap lat={latitude} lng={longitude} />
+          {<DormMap key={tempState ? 0 : (new Date()).toTimeString()} lat={latitude} lng={longitude} />}
       </div>
       <Modal
         open={open}
