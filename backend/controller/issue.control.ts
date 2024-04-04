2f32e454d3f885cd9191b56355fc434e743a13ba
Sorawit <sorawit12535@gmail.com>
2024-04-04 23:50:53 +0700

    feat: issue schema swagger

--- backend/controller/issue.control.ts
+++ backend/controller/issue.control.ts
@@ -2,23 +2,56 @@ import { User } from "@prisma/client";
 import { Request, Response } from "express";
 import generateStatusResponse from "../lib/statusResponseGenerator";
 import { z } from "zod";
+import { issueTypes } from "../lib/constant";
+import { db } from "../lib/db";
 
 
 const createIssueSchema = z.object({
-
+    type: z.enum(issueTypes),
+    title: z
+        .string()
+        .trim()
+        .min(1, { message: "Please fill title" }),
+    description: z
+        .string()
+        .trim()
+        .min(1, { message: "Please fill description" }),
+    images: z
+        .string()
+        .array()
+        .max(10, { message: "The images must not exceed 10 files" })
+        .optional(),    
 })
 
+//=============================================================================
+
+//@desc     Create reported issue
+//@route    POST /issues
+//@access   Private
 
 export const createIssue = async (req: Request, res: Response) => {
     const user: User = req.body.user
+    const body = req.body
 
     try {
         if (user.role === "Admin") {
             return res.status(403).send(generateStatusResponse(403, "Admin cannot report the issue"))
         }
 
+        const parseStatus = createIssueSchema.safeParse(body);
+        if (!parseStatus.success) console.log(parseStatus.error.issues);
+        if (!parseStatus.success) return res.status(400).send(generateStatusResponse(400, parseStatus.error.message));
 
+        const parsedBody = parseStatus.data;
 
+        const issueRes = await db.issue.create({
+            data: {
+                userId: user.id,
+                ...parsedBody
+            }
+        })
+
+        return res.status(201).send(issueRes)
         
     } catch (err) {
         return res.status(400).send(generateStatusResponse(400, err))
--- backend/routes/issue.routes.ts
+++ backend/routes/issue.routes.ts
@@ -4,6 +4,61 @@ import { createIssue } from "../controller/issue.control";
 
 const router = Router()
 
+/**
+* @swagger
+* components:
+*   schemas:
+*     Issue:
+*       type: object
+*       required:
+*           - id
+*           - userId
+*           - type
+*           - status
+*           - title
+*           - description
+*           - images
+*           - reportAt
+*       properties:
+*         id:
+*           type: string
+*           description: ID of the issue
+*           example: '194638c38ab20d7601f74e8294'
+*         userId:
+*           description: ID of the user who reported
+*           type: string
+*           example: '194638c38ab20d7601f74e8294'
+*         type:
+*           description: Type of the issue
+*           type: string
+*           enum: [Technical, Account, Content, Payment, Security, Suggestion, Other]
+*           example: Technical
+*         status:
+*           description: Status of the issue
+*           type: string
+*           enum: [Waiting, Resolved, Rejected]
+*           example: Waiting
+*         title:
+*           type: string
+*           description: Title of the issue
+*           example: Skill Issue
+*         description:
+*           type: string
+*           description: Description of this issue
+*           example: "I have skill issue, help me!"
+*         images:
+*           type: array
+*           description: Array of image links
+*           items:
+*               tyoe: string
+*           example: ["https://utfs.io/f/4a65c7f9-7bb1-4498-99bb-4148be482108-t9vzc5.png", "https://utfs.io/f/4a65c7f9-7bb1-4498-99bb-4148be482108-abcdefg.png"]
+*         reportedAt:
+*           type: string
+*           format: date-time
+*           description: Datetime that user reported the issue
+*           example: "2017-07-21T17:32:28Z"
+*/
+
 
 router.post("/", authenticateToken, createIssue)
 
