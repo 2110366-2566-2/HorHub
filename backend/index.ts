5635298e0c5fb216e9cc76ec9b58d86cd8553cf3
Sorawit <sorawit12535@gmail.com>
2024-04-20 15:25:34 +0700

    fix: minor bug

--- backend/index.ts
+++ backend/index.ts
@@ -37,7 +37,7 @@ app.use(cors({
 
 app.use(cookieParser());
 app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
-app.use(express.json())
+app.use(express.json({ limit: '50mb'}))
 //app.use(express.urlencoded());
 
 const server = createServer(app);
